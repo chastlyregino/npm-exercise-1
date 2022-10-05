c7b0ab2969f89c2fd859618b0238b13d047585c9
Chastly Regino <chastly_regino@icloud.com>
2022-10-06 06:42:10 +0800

    Added WIFI qrcode as per instructions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const qrcode = require('qrcode-terminal');
 
+console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:SocialD3$K09F;'));
+
 console.log(qrcode.generate('BEGIN:VCARD\nN:Manapat;Finn;\nTEL;TYPE=home,VOICE:09175109101\nEMAIL:innchastly.manapat@siteminder.com\nVERSION:3.0\nEND:VCARD'));
